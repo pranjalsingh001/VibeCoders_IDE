453d4f814cf329e3e125d567bf75b8b25d929c29
pranjal <pjlv1007@gmail.com>
2025-08-12 11:36:31 +0530

    Use protect function from authMiddleware in blueprint route

--- server/routes/blueprintRoutes.js
+++ server/routes/blueprintRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { generateBlueprint } = require("../controllers/blueprintController");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 // Route to generate blueprint from idea (only if logged in)
-router.post("/generate", authMiddleware, generateBlueprint);
+router.post("/generate", authMiddleware.protect, generateBlueprint);
 
 module.exports = router;
